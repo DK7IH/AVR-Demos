e5b4a9278b6debe2623de9b853b36f700ad304f1
Peter Baier <48391460+DK7IH@users.noreply.github.com>
2021-12-29 23:38:03 +0100

    Add files via upload

--- Misc/24C65 EEPROM/24c65.c
+++ Misc/24C65 EEPROM/24c65.c
@@ -1,12 +1,10 @@
 ///////////////////////////////////////////////////////////////////
-//                      24C65 EEPROM                             //
+//           24C65 EEPROM with ATmega32  16MHz                   //
 ///////////////////////////////////////////////////////////////////
 //  Compiler:         GCC (GNU AVR C-Compiler)                   //
-//  Author:           Peter Rachow (DK7IH)                       //
-//  Last change:      FEB 2019 2017                              //
-//Fuses: -U lfuse:w:0xe4:m -U hfuse:w:0xd9:m                     //
+//  Author:           Peter Baier (DK7IH)                        //
+//  Last change:      FEB 2017                                   //
 ///////////////////////////////////////////////////////////////////
-
 #include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,20 +13,18 @@
 #include <avr/io.h>
 #include <avr/sleep.h>
 #include <avr/eeprom.h>
-#include <util/delay.h>
 #include <util/twi.h>
 
-#undef F_CPU 
-#define F_CPU 80000000
+#define CPUCLK 16
 
   ///////////////////////////
  //   EEPROM 24C65        //
 ///////////////////////////
 #define EEPROMSIZE 8192
-void eeprom24c65_write(int, int, unsigned char);
-unsigned char eeprom24c65_read(int, int);
-void write_data(int, int);
-unsigned char read_data(int);
+#define DEVICE_ADDR 0xA0
+
+void eeprom24c65_write( int, unsigned char);
+unsigned char eeprom24c65_read(int);
 
   ///////////////////////////
  //        T  W  I        //
@@ -40,6 +36,22 @@ void twi_write(unsigned char u8data);
 unsigned char twi_read_nack(void);
 unsigned char twi_read_ack(void);
 unsigned char twi_get_status(void);
+
+void wait_ms(int);
+
+// Cheap & dirty delay
+void wait_ms(int ms)
+{
+    int t1, t2;
+
+    for(t1 = 0; t1 < ms; t1++)
+    {
+        for(t2 = 0; t2 < 137 * CPUCLK; t2++)
+        {
+            asm volatile ("nop" ::);
+        }   
+     }    
+}
 
   ///////////////////////////
  //        T  W  I        //
@@ -95,97 +107,80 @@ unsigned char twi_read_ack(void)
     while ((TWCR & (1<<TWINT)) == 0);
 	return TWDR;
 }
+
   ///////////////////////////
  //   EEPROM 24C65        //
 ///////////////////////////
-void eeprom24c65_write(int device, int mem_address, unsigned char value)
+void eeprom24c65_write(int mem_address, unsigned char value)
 {
    int hi_addr = mem_address >> 8;
    int lo_addr = mem_address & 0xFF;
    	
    twi_start();
-   twi_write(device); //Device address
+   twi_write(DEVICE_ADDR); //Device address
    twi_write(hi_addr);
    twi_write(lo_addr);
    twi_write(value);
    twi_stop();	
 }	
 
-unsigned char eeprom24c65_read(int device, int mem_address)
+unsigned char eeprom24c65_read(int mem_address)
 {
    int hi_addr = mem_address >> 8;
    int lo_addr = mem_address & 0xFF;
    int r;
    	
    twi_start();
-   twi_write(device);     //Device address
+   twi_write(DEVICE_ADDR);     //Device address
    twi_write(hi_addr);
    twi_write(lo_addr);
    twi_start();
-   twi_write(device + 1); //Device address + 1
+   twi_write(DEVICE_ADDR + 1); //Device address + 1
    r = twi_read_nack();
    twi_stop();	
    return r;
 }	
 
-void write_data(int mem_addr, int value)
-{
-	if(mem_addr < EEPROMSIZE)
-	{
-		eeprom24c65_write(0xA0, mem_addr, value); //EPROM #1
-	}
-	else
-	{
-		eeprom24c65_write(0xA2, mem_addr - EEPROMSIZE, value); //EPROM #2
-	}
-}	
-
-unsigned char read_data(int mem_addr)
-{
-	if(mem_addr < EEPROMSIZE)
-	{
-		return eeprom24c65_read(0xA0, mem_addr); //EPROM #1
-	}
-	else
-	{
-		return eeprom24c65_read(0xA2, mem_addr - EEPROMSIZE); //EPROM #2
-	}
-	
-	return 0;
-}	
-
 int main(void)
 {
-    int t1, x = 0;
+    int t1, x1 = 0, x2 = 0;
+    
+    DDRD = 0x03; //Set PB0, PB1 for output (control LEDs)
     	
 	//TWI
 	twi_init();
-		
-	//oled_putstring(0, 2, "Schreibe...          ", 0, 0);	
-    for(t1 = 0; t1 < 16384; t1++)
-    {
-        write_data(t1, t1);
-        //oled_putnumber(40, 5, t1, -1, 0, 0);
-        if(x > 255)
-        {
-			x = 0;
-		}
-	}
-
-	//oled_putstring(0, 2, "Lese...          ", 0, 0);
 	
-	for(t1 = 0; t1 < 16384; t1++)
+    for(t1 = 0; t1 < EEPROMSIZE; t1++)
     {
-		//oled_putnumber(20, 6, t1, -1, 0, 0);
-        //oled_putnumber(60, 6, read_data(t1), -1, 0, 0);
-        _delay_ms(10);
-	}
-			
-    
-    for(;;)
-    {
+		//Write EEPROM
+        eeprom24c65_write(t1, t1);
+        if(x1 > 255)
+        {
+			x1 = 0;
+		}
 		
-    }
+		wait_ms(200);
+		
+		//Read EEPROM
+		x2 = eeprom24c65_read(t1);
+		wait_ms(200);
+		
+		//Compare
+		if(x1 == x2)
+		{
+			PORTD &= ~(1 << PD0);
+			PORTD |= (1 << PD1);
+		}
+		else	
+		{
+			PORTD |= (1 << PD0);
+			PORTD &= ~(1 << PD1);
+		}
+		wait_ms(200);
+		PORTD |= (1 << PD0);
+		PORTD |= (1 << PD1);
+	}
+	
 	return 0;
 }
 
